41da97ff34be089d0ad8a4b81096aadbf7490371
TraceyMmboneLungahi <t.lungahi@gmail.com>
2022-08-19 17:36:41 +0300

    commit tree 5891474d17a7e2adfe889bccef30cc0a6621d3b4

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,10 @@
 class Person
   # your code here
+
+  def initialize (attribute) 
+    attribute.map do |k, v|
+      self.class.attr_accessor(k)
+      self.send("#{k}=", v)
+    end
+  end  
 end
